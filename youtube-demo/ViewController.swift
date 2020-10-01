1b9b788358b5aac879b545a05b4b8dfe5022328b
NurdauletYN <n.yerkinbekov@gmail.com>
2020-10-02 02:01:46 +0600

    initial xcode project creation

---
+++ youtube-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-demo
+//
+//  Created by macbook air on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ youtube-demoTests/youtube_demoTests.swift
@@ -0,0 +1,33 @@
+//
+//  youtube_demoTests.swift
+//  youtube-demoTests
+//
+//  Created by macbook air on 10/2/20.
+//
+
+import XCTest
+@testable import youtube_demo
+
+class youtube_demoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
